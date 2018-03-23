af4d5ea7ad21ddfa44ec604ded6fa6816aae06d3
Adrián Romero <adrianromero@users.noreply.github.com>
2018-03-24 00:50:10 +0100

    Adding styleclass to dialog labels: dialog-label

--- src/main/java/com/adr/hellocommon/dialog/DialogException.java
+++ src/main/java/com/adr/hellocommon/dialog/DialogException.java
@@ -1,6 +1,23 @@
 //    HelloCommon are basic JavaFX utilities
+//
 //    Copyright (C) 2015-2018 Adrián Romero Corchado.
-//    All Rights reserved..
+//
+//    Licensed to the Apache Software Foundation (ASF) under one
+//    or more contributor license agreements.  See the NOTICE file
+//    distributed with this work for additional information
+//    regarding copyright ownership.  The ASF licenses this file
+//    to you under the Apache License, Version 2.0 (the
+//    "License"); you may not use this file except in compliance
+//    with the License.  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing,
+//    software distributed under the License is distributed on an
+//    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//    KIND, either express or implied.  See the License for the
+//    specific language governing permissions and limitations
+//    under the License.
 
 package com.adr.hellocommon.dialog;
 
@@ -39,6 +56,7 @@ public class DialogException  {
         rootpane = new BorderPane();
         
         textmessage = new Label();
+        textmessage.getStyleClass().add("dialog-label");
         textmessage.setWrapText(true);
         BorderPane.setAlignment(textmessage, Pos.CENTER);
         BorderPane.setMargin(textmessage, new Insets(0.0, 0.0, 10.0, 0.0));
